715495f2cdc7a5b4d7672ce6d7ec59f98d352946
Mike Ward <mike@dreamcubestudios.com>
2012-02-10 13:21:58 -0800

    Created Dealer Dashboard page with user information.

---
+++ dealer-dashboard.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *Template name: Dealer Dashboard
+ */
+include (TEMPLATEPATH . '/header-products.php'); ?>
+<div id="main-subpage">		
+	<div class="columns div-slice">      
+	<h1 class="page"><?php the_title(); ?></h1>
+	
+	<div class="dealer-controls">
+	<a class="order" href="#">Order Products</a>
+	<a class="signstatus" href="#">Signout</a>
+	</div><!-- .dealer-controls -->
+	
+	<div id="dealerinfo-case"> 
+
+		<?php
+			wp_get_current_user();
+			echo '<span class="current-user">Welcome Back, ' . $current_user->company_name . '!<br />';
+			echo 'Email: ' . $current_user->user_email . '<br /></span>';
+			echo $current_user->company_name . '<br />';
+			echo $current_user->address . '<br />';
+			echo $current_user->city . '&nbsp;';
+			echo $current_user->state . '&nbsp;';
+			echo $current_user->zip . '<br />';
+		?>
+		<span class="<?php echo get_user_role(); ?>"><?php echo get_current_user_role(); ?></span>
+	</div><!-- #dealerinfo-case -->
+
+		<div id="usermessage-update">
+	<h2 class="cc-white leftpos">Announcement:</h2>
+
+	     <?php if (have_posts()) : ?>
+     <?php while (have_posts()) : the_post(); ?>							
+					
+				<h2 class="cc-lblue rightpos"><?php the_date(); ?></h2>
+     			<div class="post">
+				<div class="entry">
+					 <?php the_content(); ?> 	 
+                    <div class="clear"></div>
+                </div>                   <?php edit_post_link(__('Edit','nattywp'), '<p>', '</p>'); ?>	                 
+			</div>	    
+
+	</div><!-- #usermessage-update -->
+	
+<div id="dash-wrap">
+<img src="<?php echo get_template_directory_uri(); ?>/images/dd-bannerad.jpg" alt="" />
+
+<div class="rightpos cert-center">
+
+</div><!-- .cert-center -->
+</div><!-- #dash-wrap -->
+
+<div class="narrowcolumn-bare">
+
+
+			<?php comments_template( '', true ); ?>      	
+	<?php endwhile; ?>		
+    <?php endif; ?>	
+</div><!-- .narrowcolumn-bare -->
+
+
+
+  <div id="sidebar" class="profile">       
+    
+    <h2>Download Materials</h2>
+
+<ul class="where-about">
+<li>ABC News</li>
+<li>CBS News including "The Early Show"</li>
+<li>NBC News</li>
+<li>DIY Show "Cool Tools"</li>
+<li>DIY Show "This New House"</li>
+<li>Discovery Channel "Renovation Nation"</li>
+<li>Popular Mechanics Magazine</li>
+<li>The Washington Post</li>
+<li>Los Angeles Times</li>
+<li>San Francisco Chronicle</li>
+</ul>
+    </div>    
+
+
+<div class="clear"></div>
+<?php get_footer(); ?> 
--- functions.php
+++ functions.php
@@ -71,5 +71,27 @@ function caption_shortcode_with_credits($empty, $attr, $content) {
 	. do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
 }
 
+function get_user_role() {
+	global $current_user;
+
+	$user_roles = $current_user->roles;
+	$user_role = array_shift($user_roles);
+
+	return $user_role;
+}
+
+/**
+ * Returns the translated role of the current user. If that user has
+ * no role for the current blog, it returns false.
+ *
+ * @return string The name of the current role
+ **/
+function get_current_user_role() {
+	global $wp_roles;
+	$current_user = wp_get_current_user();
+	$roles = $current_user->roles;
+	$role = array_shift($roles);
+	return isset($wp_roles->role_names[$role]) ? translate_user_role($wp_roles->role_names[$role] ) : false;
+}
 
 ?>
